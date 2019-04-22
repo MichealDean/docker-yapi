d885fe64ba3fe357c3d3d2051d1d42eb2d9f655f
wangxh <wangxh@mail.jj.cn>
2019-04-22 14:57:23 +0800

    修改部分配置文件

--- init-mongo.js
+++ init-mongo.js
@@ -1,10 +1,10 @@
 
-db.createUser({ user: 'admin', pwd: 'admin123456', roles: [ { role: "root", db: "admin" } ] });
+db.createUser({ user: 'admin', pwd: 'JJ12345678boost', roles: [ { role: "root", db: "admin" } ] });
 
-db.auth("admin", "admin123456");
+db.auth("admin", "JJ12345678boost");
  db.createUser({
- user: 'yapi',
- pwd: 'yapi123456',
+ user: 'jj_yapi',
+ pwd: 'jj_yapi123456',
  roles: [
  { role: "dbAdmin", db: "yapi" },
  { role: "readWrite", db: "yapi" }
